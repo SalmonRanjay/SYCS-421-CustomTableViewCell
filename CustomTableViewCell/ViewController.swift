3abd8dcbbb3c94c012ee5029abcadd04e1070d20
Ranjay Salmon <ranjy23@gmail.com>
2016-02-10 19:40:41 -0500

    Initial Commit

---
+++ CustomTableViewCell/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomTableViewCell
+//
+//  Created by Ranjay Salmon on 2/10/16.
+//  Copyright © 2016 Ranjay Salmon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
